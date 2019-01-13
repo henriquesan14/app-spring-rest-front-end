428340368443882921912625f1fe177b303db140
Henrique <ricogx@hotmail.com>
2019-01-13 19:22:58 -0300

    criando interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -28,9 +28,7 @@ export class CategoriasPage {
   ionViewDidLoad() {
     this.categoriaService.findAll()
     .subscribe((response)=>{
-      this.items = response;},
-    (error)=>{
-      console.log(error);});
+      this.items = response;});
   }
 
 }
