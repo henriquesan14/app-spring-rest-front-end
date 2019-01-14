9e54bf6aa72586f67919dd37146b695497f155b1
Henrique <ricogx@hotmail.com>
2019-01-14 17:44:39 -0300

    criando page de carrinho

--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
+import { ProdutoService } from '../../services/domain/produto.service';
+import { CartService } from '../../services/domain/cart.service';
 
-/**
- * Generated class for the ProdutoDetailPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -17,15 +13,22 @@ import { ProdutoDTO } from '../../models/produto.dto';
 export class ProdutoDetailPage {
 
   item: ProdutoDTO;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public produtoService: ProdutoService,
+    public cartService: CartService) {
   }
 
   ionViewDidLoad() {
-    this.item = {
-      id: '1',
-      nome: "Mouse",
-      preco: 80.59
-    }
+    let produto_id = this.navParams.get('produto_id');
+    this.produtoService.findById(produto_id)
+    .subscribe((response)=>{
+      this.item = response;
+    },(error)=>{});
+  }
+
+  addToCart(produto: ProdutoDTO){
+    this.cartService.addProduto(produto);
+    this.navCtrl.setRoot('CartPage');
   }
 
 }
--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
-import { CategoriaDTO } from "../../models/categoria.dto";
-import { Observable } from "rxjs/Rx";
+import { ProdutoDTO } from "../../models/produto.dto";
 
 @Injectable()
 export class ProdutoService{
@@ -12,5 +11,9 @@ export class ProdutoService{
 
     findByCategoria(categoria_id: string){
         return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+    }
+
+    findById(produto_id: string){
+        return this.http.get<ProdutoDTO>(`${API_CONFIG.baseUrl}/produtos/${produto_id}`);
     }
 }
