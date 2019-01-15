1e15b336486ffb8ab8acfb5e7708eddcd422e599
Henrique <ricogx@hotmail.com>
2019-01-15 00:32:10 -0300

    buscando endereços do banco de dados

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { StorageService } from '../../services/storage.service';
+import { ClienteService } from '../../services/domain/cliente.service';
 
 
 @IonicPage()
@@ -11,44 +13,26 @@ import { EnderecoDTO } from '../../models/endereco.dto';
 export class PickAddressPage {
 
   items: EnderecoDTO[];
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public storage: StorageService,
+    public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
-    this.items = [
-      {
-        id: "1",
-        logradouro: "Rua Alexandre Toledo da Silva",
-        numero: "123",
-        complemento: null,
-        bairro: "Centro",
-        cep: "85656536",
-        cidade: {
-          id: "3",
-          nome: "São Paulo",
-          estado: {
-            id: "2",
-            nome: "São Paulo"
-          }
-        }
+    let localUser = this.storage.getLocalUser();
+    if(localUser && localUser.email){
+      this.clienteService.findByEmail(localUser.email)
+      .subscribe((response) => {
+        this.items = response['enderecos'];
       },
-      {
-        id: "1",
-        logradouro: "Rua das flores",
-        numero: "434",
-        complemento: null,
-        bairro: "Jardim",
-        cep: "78451213",
-        cidade: {
-          id: "4",
-          nome: "Teste",
-          estado: {
-            id: "8",
-            nome: "testee"
-          }
+      (error) => {
+        if(error.status==403){
+          this.navCtrl.setRoot('HomePage'); 
         }
-      }
-    ]
+      });
+    }else{
+      this.navCtrl.setRoot('HomePage');
+    }
   }
 
 }
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { HttpClient} from "@angular/common/http";
-import { Observable } from "rxjs/Rx";
 import { ClienteDTO } from "../../models/cliente.dto";
 import { API_CONFIG } from "../../config/api.config";
 import { StorageService } from "../storage.service";
@@ -11,8 +10,8 @@ export class ClienteService{
     constructor(public http:HttpClient, public storage: StorageService){
     }
 
-    findByEmail(email: string): Observable<ClienteDTO>{
-        return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+    findByEmail(email: string){
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
     }
 
     insert(obj: ClienteDTO){
