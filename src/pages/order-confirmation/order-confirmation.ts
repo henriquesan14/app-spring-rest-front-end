c10210927aa1627c5710043f8e3741f08d0a1bf4
Henrique <ricogx@hotmail.com>
2019-01-15 19:30:44 -0300

    informando pedido registrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -27,7 +27,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
-
+  codpedido: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public cartService: CartService,
@@ -59,7 +59,7 @@ export class OrderConfirmationPage {
     this.pedidoService.insert(this.pedido)
     .subscribe((response)=>{
       this.cartService.createOrClearCart();
-      console.log(response.headers.get('location'));
+      this.codpedido = this.extractId(response.headers.get('location'));
     },(error)=>{
       if(error.status==403){
         this.navCtrl.setRoot('HomePage');
@@ -69,6 +69,15 @@ export class OrderConfirmationPage {
 
   back(){
     this.navCtrl.setRoot('CartPage');
+  }
+
+  private extractId(location: string): string{
+    let position = location.lastIndexOf('/');
+    return location.substring(position+1, location.length);
+  }
+
+  home(){
+    this.navCtrl.setRoot('CategoriasPage');
   }
 
 }
